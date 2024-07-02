5d2eab0c6392613e1b9f96bc63e5c68a4744df42
Penpitcha Ketpetch <144467890+primpenpiz@users.noreply.github.com>
2024-07-02 11:16:14 +0700

    Update variables.tf

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -1,59 +1,12 @@
-variable "vpc-name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
-variable "igw-name" {
-  description = "Name of the Internet Gateway"
-  type        = string
-}
-
-variable "rt-name2" {
-  description = "Name of the route table"
-  type        = string
-}
-
-variable "subnet-name" {
-  description = "Name of the first subnet"
-  type        = string
-}
-
-variable "subnet-name2" {
-  description = "Name of the second subnet"
-  type        = string
-}
-
-variable "security-group-name" {
-  description = "Name of the security group"
-  type        = string
-}
-
-variable "iam-role-eks" {
-  description = "IAM role for EKS"
-  type        = string
-}
-
-variable "iam-role-node" {
-  description = "IAM role for EKS nodes"
-  type        = string
-}
-
-variable "iam-policy-eks" {
-  description = "IAM policy for EKS"
-  type        = string
-}
-
-variable "iam-policy-node" {
-  description = "IAM policy for EKS nodes"
-  type        = string
-}
-
-variable "cluster-name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
-
-variable "eksnode-group-name" {
-  description = "Name of the EKS node group"
-  type        = string
-}
+variable "vpc-name" {}
+variable "igw-name" {}
+variable "rt-name2" {}
+variable "subnet-name" {}
+variable "subnet-name2" {}
+variable "security-group-name" {}
+variable "iam-role-eks" {}
+variable "iam-role-node" {}
+variable "iam-policy-eks" {}
+variable "iam-policy-node" {}
+variable "cluster-name" {}
+variable "eksnode-group-name" {} 
